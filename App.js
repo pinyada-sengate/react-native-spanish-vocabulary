8e1e20c9a74d3ef6521a836b93890eac153b4f1f
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-27 13:23:54 +0100

    implement react native stack

--- App.js
+++ App.js
@@ -1,12 +1,25 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import CategoriesScreen from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: { backgroundColor: "#351401" },
+          headerTintColor: "white",
+          contentStyle: { backgroundColor: "#32f25" },
+        }}
+      >
+        <Stack.Screen name="Categories" component={CategoriesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
