e84359111501f284ded04883b0d50c485a72a506
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-27 13:01:20 +0100

    category data structure

---
+++ data/dummy-data.js
@@ -0,0 +1,14 @@
+import Category from "../models/category";
+
+export const CATEGORIES = [
+  new Category("c1", "Animal"),
+  new Category("c2", "Fruit"),
+  new Category("c3", "Vegetable"),
+  new Category("c4", "Vehicle"),
+  new Category("c5", "Profession"),
+  new Category("c6", "House"),
+  new Category("c7", "bedroom"),
+  new Category("c8", "Living Room"),
+  new Category("c9", "Bathroom"),
+  new Category("c10", "Kitchen"),
+];
