394a19544111f9cfef5707948a4a54c0a9a6bc1c
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-27 17:20:44 +0100

    flashcard model

---
+++ models/flashcard.js
@@ -0,0 +1,11 @@
+class Flashcard {
+  constructor(id, categoryIds, imageUrl, es, en) {
+    this.id = id;
+    this.categoryIds = categoryIds;
+    this.imageUrl = imageUrl;
+    this.es = es;
+    this.en = en;
+  }
+}
+
+export default Flashcard;
