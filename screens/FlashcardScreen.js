8f5c0b5aca2eca48e1483e98ca55d89d47bc69f6
Pinyada Sengate <pinyada.sengate@gmail.com>
2025-01-27 20:24:29 +0100

    Show category title in flashcard screen

--- screens/FlashcardScreen.js
+++ screens/FlashcardScreen.js
@@ -1,13 +1,23 @@
-import { FLASHCARDS } from "../data/dummy-data";
+import { useLayoutEffect } from "react";
 
+import { CATEGORIES, FLASHCARDS } from "../data/dummy-data";
 import FlashcardList from "../components/Flashcard/FlashcardList";
 
-function FlashcardScreen({ route }) {
+function FlashcardScreen({ route, navigation }) {
   const catId = route.params.categoryId;
 
   const displayedFlashcards = FLASHCARDS.filter((flashcard) => {
     return flashcard.categoryIds.indexOf(catId) >= 0;
   });
+
+  useLayoutEffect(() => {
+    const category = CATEGORIES.find((category) => category.id === catId);
+
+    navigation.setOptions({
+      title: category.title,
+    });
+  }, [catId, navigation]);
+
   return <FlashcardList items={displayedFlashcards} />;
 }
 
